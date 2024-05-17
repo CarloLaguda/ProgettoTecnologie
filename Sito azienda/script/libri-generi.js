d42f9b93b262edca31eb8b42064f09870992eaf6
mattaLeo <159021786+mattaLeo@users.noreply.github.com>
2024-05-17 19:28:38 +0200

    v0.1
    Aggiunta il caricamento dei libri nella pagina della libreria, con la creazione dei rispettivi div, inoltre ognuno di questi libri ah un pulsante elimina, che elimina l'elemento non solo graficamente, ma anche lato back-end

--- Sito azienda/script/libri-generi.js
+++ Sito azienda/script/libri-generi.js
@@ -1,24 +1,55 @@
 import { getBooks, deleteBook, createBook } from "../script.js";
 
-const libri = document.getElementById("libri");
+const libri = document.getElementById("books");
 
 async function loadBooks() {
     let data = await getBooks();
 
     data.forEach(bookData => {
         const book = document.createElement("div");
-        const title = document.createElement("h3");
+        book.classList.add("book")
+
+        const img = document.createElement("img")
+        img.src = "#"
+        img.alt = "Immagine del libro"
+        book.appendChild(img)
+
+        const desc = document.createElement("div");
+        const title = document.createElement("h3")
         title.innerHTML = bookData.title;
+        desc.appendChild(title)
+        const description = document.createElement("p")
+        description.innerHTML = "Breve desc"
+        desc.appendChild(description)
+        book.appendChild(desc)
 
-        const genere = document.createElement("h4");
-        genere.innerHTML = bookData.genreName;
+        const stella = document.createElement("div")
+        stella.classList.add("stella")
+        const btnStella = document.createElement("button")
+        btnStella.innerHTML = "Stellina"
+        stella.appendChild(btnStella)
+        book.appendChild(stella)
 
-        const prezzo = document.createElement("h4");
-        prezzo.innerHTML = bookData.price;
-
-        book.appendChild(title);
-        book.appendChild(genere);
-        book.appendChild(prezzo);
+        const buttons = document.createElement("div")
+        buttons.classList.add("buttons")
+        const btnModifica = document.createElement("button")
+        btnModifica.classList.add("btn")
+        btnModifica.innerHTML = "modifica"
+        buttons.appendChild(btnModifica)
+        const btnElimina = document.createElement("button")
+        btnElimina.value = bookData.id
+        btnElimina.addEventListener("click", function(){
+            deleteBook(parseInt(this.value))
+            this.parentElement.parentElement.remove()
+        })
+        btnElimina.classList.add("btn")
+        btnElimina.innerHTML = "elimina"
+        buttons.appendChild(btnElimina)
+        const btnSegnala = document.createElement("button")
+        btnSegnala.classList.add("btn")
+        btnSegnala.innerHTML = "segnala"
+        buttons.appendChild(btnSegnala)
+        book.appendChild(buttons)
 
         libri.appendChild(book); 
     });
