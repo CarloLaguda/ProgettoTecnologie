5c728b4e83de150f153c3779fcb70eeb4822cb2a
wheelchaired_potato <159021761+LaPatataSullaSedia@users.noreply.github.com>
2024-05-17 10:57:35 +0200

    Add files via upload

---
+++ ProgettoTecnologie-main/Sito azienda/script.js
@@ -0,0 +1,65 @@
+const URL = 'https://librarymanagementpw.azurewebsites.net'
+
+// lista libri
+const getBooks = async () => {
+	const res = await fetch(`${URL}/api/Book`)
+	const data = await res.json()
+	return data
+}
+
+// crea un libro
+const createBook = async (book) => {
+	const res = await fetch(`${URL}/api/Book`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(book),
+	})
+	return res
+}
+
+// cancella un libro
+const deleteBook = async (id) => {
+	const res = await fetch(`${URL}/api/Book/${id}`, {
+		method: 'DELETE',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	})
+	return res
+}
+
+// lista generi
+const getGenres = async () => {
+	const res = await fetch(`${URL}/api/Genre`)
+	const data = await res.json()
+	return data
+}
+
+// crea un genere
+const createGenre = async (genre) => {
+	const res = await fetch(`${URL}/api/Genre`, {
+	    method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(genre),
+	})
+    return res
+}
+
+// cancella un genere
+const deleteGenre = async (id) => {
+	const res = await fetch(`${URL}/api/Genre/${id}`, {
+		method: 'DELETE',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	})
+	return res
+}
+
+console.log(getBooks())
+
+export {deleteBook, deleteGenre, createGenre, createBook, getGenres, getBooks}
